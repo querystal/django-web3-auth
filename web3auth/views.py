d4638852e5cac851151bb1fc8ba2cad76937ac77
Denis Bobrov <delneg@Yandex.ru>
2018-06-25 00:21:32 +0300

    added signup template to example, package, urls Fixed signup view

--- web3auth/views.py
+++ web3auth/views.py
@@ -12,6 +12,8 @@ from django.utils.translation import ugettext_lazy as _
 from web3auth.settings import app_settings
 
 import json
+
+
 @require_http_methods(["GET", "POST"])
 def login_api(request):
     if request.method == 'GET':
@@ -41,6 +43,7 @@ def login_api(request):
             else:
                 return JsonResponse({'success': False, 'error': json.loads(form.errors.as_json())})
 
+
 @require_http_methods(["POST"])
 def signup_api(request):
     if not app_settings.WEB3AUTH_SIGNUP_ENABLED:
@@ -58,41 +61,21 @@ def signup_api(request):
         return JsonResponse({'success': False, 'error': json.loads(form.errors.as_json())})
 
 
-def login_view(request, template_name='web3auth/login.html'):
-    if request.method == 'POST':
-        token = request.session['login_token']
-        form = LoginForm(token, request.POST)
-        if form.is_valid():
-            if form.user is not None:
-                del request.session['login_token']
-                login(request, form.user)
-                return redirect(request.GET.get('next') or request.POST.get('next') or settings.LOGIN_REDIRECT_URL)
-            else:
-                request.session['ethereum_address'] = recover_to_addr(token, form.cleaned_data['signature'])
-                return redirect(signup_view)
-    else:
-        token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(32))
-        request.session['login_token'] = token
-        form = LoginForm(token)
-    return render(request,
-                  template_name,
-                  {'form': form,
-                   'login_token': token})
-
-
+@require_http_methods(["GET", "POST"])
 def signup_view(request, template_name='web3auth/signup.html'):
-    if request.method == 'POST':
-        ethereum_address = request.session['ethereum_address']
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            del request.session['ethereum_address']
-            user = form.save(commit=False)
-            user.username = ethereum_address
-            user.save()
-            login(request, user)
-            return redirect(request.GET.get('next') or request.POST.get('next') or settings.LOGIN_REDIRECT_URL)
+    form = SignupForm()
+    if not app_settings.WEB3AUTH_SIGNUP_ENABLED:
+        form.add_error(None, _("Sorry, signup's are currently disabled"))
     else:
-        form = SignupForm()
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                addr_field = app_settings.WEB3AUTH_USER_ADDRESS_FIELD
+                setattr(user, addr_field, form.cleaned_data[addr_field])
+                user.save()
+                login(request, user, 'web3auth.backend.Web3Backend')
+                return redirect(request.GET.get('next') or request.POST.get('next') or settings.LOGIN_REDIRECT_URL)
     return render(request,
                   template_name,
                   {'form': form})
