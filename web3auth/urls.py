93cfea630e45b648948a1d8fa1c0f031824352a9
Jakub Boukal <www.bagr@gmail.com>
2021-03-16 00:09:25 +0100

    Travis happy

--- web3auth/urls.py
+++ web3auth/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from web3auth import views
 
+app_name = 'web3auth'
+
 urlpatterns = [
     url(r'^login_api/$', views.login_api, name='web3auth_login_api'),
     url(r'^signup_api/$', views.signup_api, name='web3auth_signup_api'),
